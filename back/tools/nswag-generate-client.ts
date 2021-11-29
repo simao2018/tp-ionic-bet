1f9c4f6422f54b98092d7149721ba64f8b0085b2
simao2018 <chancelsim@outlook.fr>
2021-11-29 09:45:42 +0100

    edit swagger gen

--- back/tools/nswag-generate-client.ts
+++ back/tools/nswag-generate-client.ts
@@ -5,7 +5,7 @@ import * as NodeHelpers from 'child-process-promise';
 
 const apiSwaggerUrl = 'http://localhost:3018/api/docs/swagger.json';
 const swaggerCodegenConfig = path.join(__dirname, 'swagger-codegen-config.json');
-const swaggerFrontOutput = path.join(__dirname, '../..', 'front', 'src', 'providers', 'api-client.generated');
+const swaggerFrontOutput = path.join(__dirname, '../..', 'ionic-bet', 'src', 'providers', 'api-client.generated');
 
 if (!fs.existsSync(swaggerCodegenConfig)) {
     console.error(colors.red(`Impossible de générer le code client : le fichier de configuration ${swaggerCodegenConfig} n'existe pas !`));
