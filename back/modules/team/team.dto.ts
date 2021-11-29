c76c54f492a7a3888d6c8a6a2ff44a14e7aef71c
simao2018 <chancelsim@outlook.fr>
2021-11-29 09:44:10 +0100

    edit team

--- back/modules/team/team.dto.ts
+++ back/modules/team/team.dto.ts
@@ -6,5 +6,8 @@ export class TeamDto {
 
     @ApiPropertyOptional()
     label?: string;
+
+    @ApiPropertyOptional()
+    logo?: string;
 }
 
--- back/modules/team/team.entity.ts
+++ back/modules/team/team.entity.ts
@@ -8,15 +8,20 @@ export class Team extends BaseEntity {
     @Column('varchar', { name: 'label', length: 36, nullable: false })
     label: string;
 
+    @Column('varchar', { name: 'logo', length: 255, nullable: true })
+    logo?: string;
+
     toDto(): TeamDto {
         return {
             id: this.id,
             label: this.label,
+            logo: this.logo
         }
     }
 
     fromDto(dto: TeamDto) {
         this.id = dto.id;
         this.label = dto.label;
+        this.logo = dto.logo;
     }
 }
