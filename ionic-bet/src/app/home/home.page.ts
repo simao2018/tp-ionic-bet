3ba29608126c11d3b037e10ff4ab480a28baa9ee
simao2018 <chancelsim@outlook.fr>
2021-11-29 09:47:27 +0100

    init list match

--- ionic-bet/src/app/home/home.page.ts
+++ ionic-bet/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BetService, MatchDto } from '../../providers/api-client.generated';
+import { MatchService } from '../../providers/api-client.generated/api/match.service';
+import { GetMatchList } from '../../providers/api-client.generated/model/getMatchList';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  matchList: MatchDto[] = [];
+  constructor(
+    private matchService: MatchService,
+  ) { }
 
+  async ngOnInit() {
+    console.log(`load api request`);
+    const response: GetMatchList = await this.matchService.getMatchs().toPromise();
+    if (response.success)
+      console.log('matchs : ', response.matchs);
+
+    this.matchList = response.matchs;
+
+  }
+
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
 }
